1e1a1bfd39e990d629a0508f12fe63a033d01881
XORandom <XORandom@users.noreply.github.com>
2025-12-05 18:20:18 +0300

    create main menu ui
    Набросал UI главного меню

---
+++ complete_scenes/ui/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	pass # Replace with function body.
---
+++ complete_scenes/ui/ui_elements/custom_button.gd
@@ -0,0 +1,2 @@
+class_name CustomButton
+extends Button
