1dada4a025d4b540462dfdacd22b9e64763d75fb
XORandom <XORandom@users.noreply.github.com>
2025-12-07 23:27:24 +0300

    create doom-like melting effect

--- complete_scenes/ui/main_menu.gd
+++ complete_scenes/ui/main_menu.gd
@@ -4,14 +4,20 @@ extends Control
 ## Куда игрок должен переходить, после нажатия кнопки играть
 @export_file("*.tscn") var game_scene_path : String = "uid://dkar06pst402u"
 
+@onready var sub_viewport_container: SubViewportContainer = $SubViewportContainer
+
 
 func _on_start_pressed() -> void:
-	SceneManager.call_deferred("change_scene", game_scene_path)
-	pass # Replace with function body.
+	await get_tree().create_timer(1).timeout
+	var tween: Tween = create_tween()
+	tween.tween_property(sub_viewport_container.get_material(), "shader_parameter/movement", 1., 2.0)
+	#SceneManager.call_deferred("change_scene", game_scene_path)
+	await tween.finished
+	SceneManager.change_scene(game_scene_path)
 
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	pass
 
 
 func _on_quit_pressed() -> void:
---
+++ complete_scenes/ui/sub_viewport_container.gd
@@ -0,0 +1,6 @@
+extends SubViewportContainer
+
+
+func create_doom_effect() -> void:
+	set("shader_parameter/movement", 1.)
+	pass
